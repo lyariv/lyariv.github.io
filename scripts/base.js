d2abbf80b86a8cd34bc51d472172d0965e228f30
lyariv <79592520+lyariv@users.noreply.github.com>
2022-12-03 11:51:18 -0500

    Upload script to accomodate responsive design

--- scripts/base.js
+++ scripts/base.js
@@ -1,13 +1,11 @@
 function toggleNavPanel(){
-    var panel = document.getElementById('nav'), navarrow = document.getElementById("hamburger"), maxH="400px";
-    if(panel.style.height == maxH){
-        panel.style.height = "0px";
-        panel.style.display = "none";
-        navarrow.innerHTML = "&#9776;";
+    var panel = document.getElementById('nav-dialog'), 
+    navarrow = document.getElementById("hamburger"), maxH="400px";
+
+    if(panel.open){
+        panel.hide();
     } else {
-        panel.style.height = maxH;
-        panel.style.display = 'block';
-        navarrow.innerHTML = "&#9652;";
+        panel.showModal();
     }
 }
 
